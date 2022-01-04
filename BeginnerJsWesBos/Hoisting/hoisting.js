a6ba7655c26c19f19750a2f2422a4f7f3a7669a4
Gregg Cline <ggcline82@gmail.com>
2022-01-04 15:11:44 -0800

    worked on scope and Hoisting in this module

---
+++ BeginnerJsWesBos/Hoisting/hoisting.js
@@ -0,0 +1,24 @@
+
+sayHi()
+
+function sayHi() {
+console.log('Hey');
+}
+
+// Hoisting in javaScript will take your functions and basicially move them to the top and still run them 
+//so you can run a function before it exsits 
+
+//Why is this useful? 
+//Perfer to find oput what does this file do and how does it do it? 
+
+
+
+// There is also Variable Hoisting 
+
+var age;
+
+console.log(age);
+
+var age = 10;
+
+//Hoisting will move the variable or function to the top so it can still run 
--- BeginnerJsWesBos/scopePractice/scope.js
+++ BeginnerJsWesBos/scopePractice/scope.js
@@ -22,11 +22,61 @@
 // go();
 
 
-function isCool(name) {
-	let cool; 
-	if (name === 'Gregg') {
-		cool = true;
-	}
-	console.log(cool);
-	return cool;
+// function isCool(name) {
+// 	let cool; 
+// 	if (name === 'Gregg') {
+// 		cool = true;
+// 	}
+// 	console.log(cool);
+// 	return cool;
+// }
+
+//Block Scoping is whatever you have inside the  curly brackets is what your varaible is 
+//scoped to. 
+// Var variable is function scoped not block scoped 
+// What are the differences between block scope, global scoped and function scoped
+// Basically Scoped is saying there are barriers that can be crossed or not between your 
+// entire application aka Global Scoped, you have function scoped and block scope. 
+
+
+
+// for(let i = 0; i < 10; i++)  {
+// 	console.log(i);
+// }
+
+
+
+
+// Example of scope look up 
+
+const dog = 'snickers'; 
+
+function logDog() {
+	console.log(dog);
+
 }
+function go() {
+	const dog = 'sonny';
+	logDog();
+}
+
+go();
+
+
+//This is a part of JavaScript Lexical Scoping or static scoping 
+// Scope look up or variable look up is looking for where they are defined not where it is ran
+
+
+//Best Practices for creating variables
+ // -- Dont make global variable
+ //-- Function scoping you can make a function inside of a function that is allowed but not very common
+ 
+
+function sayHi(name) {
+	function yell() {
+		console.log(name.toUpperCase())
+	}
+	yell();
+}
+
+yell();
