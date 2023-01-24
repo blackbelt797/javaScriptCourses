b35c26eb5804f83633a9d4428a743b8df85977f8
Gregg Cline <ggcline82@gmail.com>
2023-01-24 08:37:26 -0800

    commiting changes frome the last time I worked on this coursejumping back into it

---
+++ BeginnerJsWesBos/Module8DataTypes/Video46ArrayCardio/arrayCardio.js
@@ -0,0 +1,26 @@
+console.log('it works');
+
+const names = ['Gregg', 'Megan', 'Owen', 'Declan'];
+
+// This is how to loop through an array
+names.forEach((name) => {
+ console.log(name);
+});
+
+//Array Prototypes utilitiies
+
+/* 
+Static Methods
+*/
+
+// Array.of();
+
+//Make a function that creates a range from x to y with Array.from();
+
+//Check if the least array you created is really an array with Array.isArray();
+
+// Take the meats object and make three arrays with Object.entries(), Object.keys, Object.values()
+
+/* 
+Instance Methods
+*/
